0cb1da7981c0682afa4b48cc074a285bfced8e29
Gian Marco Todesco <gianmarco.todesco@gmail.com>
2020-01-16 12:00:24 +0100

    .

--- epicicli.1.js
+++ epicicli.1.js
@@ -1,75 +1,162 @@
-const canvas = document.getElementById('c');
-const ctx = canvas.getContext('2d');
-let width, height;
+// gian marco todesco - gianmarco.todesco@gmail.com
+// Lezione "Curve Animate", 15 gennaio 2020
 
-function mainLoop() {
-    width = canvas.width;
-    height = canvas.height;
-    ctx.clearRect(0,0,width,height);
-    ctx.save();
-    ctx.translate(width/2,height/2);
-    draw();
-    ctx.restore();
-    requestAnimationFrame(mainLoop);
+'use strict'
+
+const canvas = document.getElementById('c')
+const ctx = canvas.getContext('2d')
+let width, height
+
+// ciclo principale: cancella il canvas e disegna il nuovo fotogramma
+function mainLoop () {
+    width = canvas.width
+    height = canvas.height
+    ctx.clearRect(0, 0, width, height)
+    ctx.save()
+    ctx.translate(width / 2, height / 2)
+    draw()
+    ctx.restore()
+    requestAnimationFrame(mainLoop)
 }
-requestAnimationFrame(mainLoop);
+requestAnimationFrame(mainLoop)
 
-function fillCircle(x,y,r) {
-    ctx.beginPath();
-    ctx.moveTo(x+r,y);
-    ctx.arc(x,y,r,0,2*Math.PI);
-    ctx.stroke();
-    ctx.fill();
-}
+// --------------------------------------------------------
+// parametri:
+// --------------------------------------------------------
 
-curve = [];
-let r1 = 120, r2 = 50, r3 = 20;
-    
-function draw() {
-    const time = performance.now() * 0.001;
+// eslint-disable-next-line prefer-const
+let omega = 0.6 // velocità del secondo cerchio (radianti/secondo)
 
-    let phi1 = Math.PI*2*time * 0.1;
-    let phi2 = phi1 + phi1 * r1/r2;
-    let phi3 = phi2 + phi2 * r2/r3;
+// raggi dei vari cerchi.
+// n.b. l'ultimo cerchio rappresenta il punto blu è ha raggio zero 
+// eslint-disable-next-line prefer-const
+let radii = [120, 50, 20, 0]
 
-    let x1 = Math.cos(phi1)*(r1+r2);
-    let y1 = Math.sin(phi1)*(r1+r2);
-    
-    let x2 = x1 + Math.cos(phi2)*(r2+r3);
-    let y2 = y1 + Math.sin(phi2)*(r2+r3);
-    
-    let x3 = x2 + Math.cos(phi3)*r3;
-    let y3 = y2 + Math.sin(phi3)*r3;
-    
-    ctx.fillStyle = "rgb(200,200,200,0.5)";
-    ctx.strokeStyle = "gray";
-    ctx.lineWidth = 1;
-    fillCircle(0,0,r1);
-    fillCircle(x1,y1,r2);
-    fillCircle(x2,y2,r3);
+// eslint-disable-next-line prefer-const
+let maxCurveLength = 1000 // massimo numero di punti della linea rossa
 
-    ctx.beginPath();
-    ctx.moveTo(0,0);
-    ctx.lineTo(x1,y1);
-    ctx.lineTo(x2,y2);
-    ctx.lineTo(x3,y3);
-    ctx.stroke();
+// --------------------------------------------------------
+// variabili
+// --------------------------------------------------------
 
-    ctx.fillStyle = "blue";
-    fillCircle(x3,y3,3);
+// posizioni correnti
+let positions = []
 
-    
-    curve.push([x3,y3]);
-    var m = 1000;
-    if(curve.length>m) curve.splice(0,curve.length-m);
-    if(curve.length>2) {
-        ctx.beginPath();
-        ctx.moveTo(curve[0][0], curve[0][1]);
-        for(let i=1; i<curve.length; i++) 
-            ctx.lineTo(curve[i][0], curve[i][1]);
-        ctx.strokeStyle = "red";
-        ctx.lineWidth = 2;
-        ctx.stroke();
+// la traccia lasciata dal punto blu
+const curve = []
+
+// --------------------------------------------------------
+// funzione principale di disegno
+// --------------------------------------------------------
+
+function draw () {
+    // aggiorna le posizioni
+    compute()
+
+    // disegno i cerchi
+    ctx.fillStyle = 'rgb(200,200,200,0.5)'
+    ctx.strokeStyle = 'gray'
+    ctx.lineWidth = 1
+    for (let i = 0; i < positions.length - 1; i++) {
+        fillCircle(positions[i][0], positions[i][1], radii[i])
     }
-    
+
+    // disegno la linea che congiunge i centri
+    ctx.beginPath()
+    ctx.lineWidth = 2
+    ctx.strokeStyle = 'teal'
+    ctx.moveTo(0, 0)
+    for (let i = 1; i < positions.length; i++) {
+        ctx.lineTo(positions[i][0], positions[i][1])
+    }
+    ctx.stroke()
+
+    // disegno i centri dei cerchi
+    ctx.fillStyle = 'black'
+    ctx.strokeStyle = 'gray'
+    ctx.lineWidth = 1
+    for (let i = 0; i < positions.length - 1; i++) {
+        fillCircle(positions[i][0], positions[i][1], 2)
+    }
+
+    // il punto blu
+    ctx.fillStyle = 'blue'
+    const k = positions.length - 1
+    fillCircle(positions[k][0], positions[k][1], 3)
+
+    // la traccia rossa
+    drawCurve()
+}
+
+// --------------------------------------------------------
+// aggiorna le posizioni in base al tempo
+// --------------------------------------------------------
+
+function compute () {
+    // tempo in secondi dall'inizio
+    const time = performance.now() * 0.001
+
+    // numero di cerchi
+    // (tolgo uno perchè il punto blu non è un cerchio)
+    const m = radii.length - 1
+
+    // calcolo gli angoli di rotazione dei vari
+    // cerchi (a partire dal secondo: il primo è fermo)
+    const angles = []
+    angles.push(time * omega)
+    for (let i = 1; i < m; i++) {
+        angles.push(angles[i - 1] * (1 + radii[i - 1] / radii[i]))
+    }
+
+    // calcolo la posizione dei centri
+    positions = []
+    var x = 0; var y = 0 // il primo cerchio è centrato sull'origine
+    positions.push([x, y])
+    for (let i = 0; i < m; i++) {
+    // distanza fra il centro del cerchio i-esimo e
+    // del cerchio (i+1)-esimo
+        var d = radii[i] + radii[i + 1]
+        var phi = angles[i]
+        x += Math.cos(phi) * d
+        y += Math.sin(phi) * d
+        positions.push([x, y])
+    }
+
+    // il punto blu lascia una traccia
+    addPointToCurve(x, y)
+}
+
+// --------------------------------------------------------
+// altre funzioni di servizio
+// --------------------------------------------------------
+
+// aggiunge un punto alla traccia
+// se necessario cancella i punti più vecchi
+function addPointToCurve (x, y) {
+    curve.push([x, y])
+    if (curve.length > maxCurveLength) {
+        curve.splice(0, curve.length - maxCurveLength)
+    }
+}
+
+// disegna la curva rossa
+function drawCurve () {
+    if (curve.length < 2) return
+    ctx.beginPath()
+    ctx.moveTo(curve[0][0], curve[0][1])
+    for (let i = 1; i < curve.length; i++) {
+        ctx.lineTo(curve[i][0], curve[i][1])
+    }
+    ctx.strokeStyle = 'red'
+    ctx.lineWidth = 2
+    ctx.stroke()
+}
+
+// disegna un disco
+function fillCircle (x, y, r) {
+    ctx.beginPath()
+    ctx.moveTo(x + r, y)
+    ctx.arc(x, y, r, 0, 2 * Math.PI)
+    ctx.stroke()
+    ctx.fill()
 }
--- epicicli.2.js
+++ epicicli.2.js
@@ -1,197 +1,257 @@
-const canvas = document.getElementById('c');
-const ctx = canvas.getContext('2d');
-let width, height;
+// gian marco todesco - gianmarco.todesco@gmail.com
+// Lezione "Curve Animate", 15 gennaio 2020
 
-function mainLoop() {
-    width = canvas.width;
-    height = canvas.height;
-    ctx.clearRect(0,0,width,height);
-    ctx.save();
-    ctx.translate(width/2, height/2);
-    draw();
-    ctx.restore();
-    requestAnimationFrame(mainLoop);
+/* eslint-disable prefer-const */
+/* eslint-disable eqeqeq */
+
+'use strict'
+
+const canvas = document.getElementById('c')
+const ctx = canvas.getContext('2d')
+let width, height
+
+// ciclo principale: cancella il canvas e disegna il nuovo fotogramma
+function mainLoop () {
+    width = canvas.width
+    height = canvas.height
+    ctx.clearRect(0,0,width,height)
+    ctx.save()
+    ctx.translate(width/2, height/2)
+    draw()
+    ctx.restore()
+    requestAnimationFrame(mainLoop)
 }
-requestAnimationFrame(mainLoop);
+requestAnimationFrame(mainLoop)
 
+// --------------------------------------------------------
+// parametri
+// --------------------------------------------------------
 
+// velocità di rotazione del primo epiciclo
+let omega = 0.3
 
-var dft = [];
+let maxTailLength = 1000
 
-function analyze(crv) {
-    var n = crv.length;
-    var d = Math.floor(n/2);
-    dft = [];
-    for(var i=0;i<n;i++) {
-        var phi = 2*Math.PI*(i-d)/n;
-        var re = 0, im = 0;
-        for(var j=0; j<n; j++) {
-            var re1 = crv[j][0], im1 = crv[j][1];
-            var re2 = Math.cos(phi*j), im2 = -Math.sin(phi*j);
-            var re3 = re1*re2 - im1*im2;
-            var im3 = re1*im2 + re2*im1;
-            re += re3;
-            im += im3;
+// --------------------------------------------------------
+// variabili
+// --------------------------------------------------------
+
+// traccia lasciata dal punto blu
+let tail = []
+
+// curva disegnata dall'utente
+let targetCrv = []
+
+// modalita': se stroking vale true allora l'utente sta disegnando
+let stroking = false
+
+// dft = Discrete Fourier Transform
+const dft = {
+    center: {},
+    items: []
+}
+
+// centro e raggio dei vari epicicli
+let circles = []
+
+// --------------------------------------------------------
+// gestione click&drag del mouse
+// --------------------------------------------------------
+let offx = 0
+let offy = 0
+function onMouseDown (e) {
+    // l'utente ha fatto click nel canvas.
+    // comincio a seguire globalmente gli eventi di mouse-move e mouse-up
+    document.addEventListener('mousemove', onDrag)
+    document.addEventListener('mouseup', onRelease)
+    // mi ricordo la differenza fra coordinate rispetto al canvas (offsetX,offsetY)
+    // e rispetto al documento (e.clientX, e.clientY)
+    offx = e.offsetX - e.clientX
+    offy = e.offsetY - e.clientY
+    // informo il programma che l'utente ha cominciato a disegnare
+    strokeStart()
+}
+function onDrag (e) {
+    // il mouse si è mosso (dopo il click)
+    // calcolo le coordinate del mouse rispetto al centro del canvas
+    var x = e.clientX + offx - canvas.width/2 
+    var y = e.clientY + offy - canvas.height/2
+    // informo il programma che l'utente ha prolungato il disegno fino al punto (x,y)
+    stroke(x, y)
+}
+function onRelease (e) {
+    // l'utente ha finito di disegnare. smetto di seguire mouse-move e mouse-up
+    document.removeEventListener('mousemove', onDrag)
+    document.removeEventListener('mouseup', onRelease)
+    // informo il programma che il disegno è finito
+    strokeEnd()
+}
+// tutte le volte che l'utente fa click nel canvas chiamo la funzione onMouseDown
+canvas.addEventListener('mousedown', onMouseDown)
+
+// l'utente ha cominciato a tracciare una curva
+function strokeStart () {
+    // cancello le curve precedenti
+    tail = []
+    targetCrv = []
+    // cambio lo stato del programa
+    stroking = true
+}
+// l'utente ha disegnato un pezzetto di curva
+function stroke (x, y) {
+    // aggiungo il punto (x,y) a targetCrv solo se non è troppo vicino all'ultimo
+    // punto disegnato
+    const m = targetCrv.length
+    if (m > 0) {
+        const lastPoint = targetCrv[m - 1]
+        const dx = lastPoint[0] - x
+        const dy = lastPoint[1] - y
+        if (dx * dx + dy * dy < 50) return
+    }
+    targetCrv.push([x, y])
+}
+// l'utente ha finito di disegnare: calcolo la DFT (se la curva disegnata è abbastanza lunga)
+function strokeEnd () {
+    if (targetCrv.length > 10) {
+        computeDft(targetCrv)
+    } else {
+        targetCrv = []
+    }
+    stroking = false
+}
+
+// --------------------------------------------------------
+// Questa funzione calcola la DFT a partire da una lista di
+// punti (x,y)
+// --------------------------------------------------------
+function computeDft (crv) {
+    const n = crv.length
+    const d = Math.floor(n / 2)
+    dft.items = []
+    for (let k = -d; k < -d + n; k++) {
+        const phi = 2 * Math.PI * k / n
+        let re = 0
+        let im = 0
+        for (let j = 0; j < n; j++) {
+            const re1 = crv[j][0]
+            const im1 = crv[j][1]
+            const re2 = Math.cos(phi * j)
+            const im2 = -Math.sin(phi * j)
+            var re3 = re1 * re2 - im1 * im2
+            var im3 = re1 * im2 + re2 * im1
+            re += re3
+            im += im3
         }
-        dft.push([re,im]);
-    }
-}
-
-
-function drawCircle(x,y,r) {
-    ctx.beginPath();
-    ctx.moveTo(x+r,y);
-    ctx.arc(x,y,r,0,2*Math.PI);
-    ctx.stroke();
-}
-
-function drawArrow(x0,y0, x1,y1) {
-    ctx.beginPath();
-    ctx.moveTo(x0,y0);
-    ctx.lineTo(x1,y1);
-    ctx.stroke();
-}
-
-var tail = [];
-var targetCrv;
-var stroking = false;
-
-var mousex, mousey,dx,dy;
-function onMouseDown(e) {
-    document.addEventListener('mousemove', onDrag);
-    document.addEventListener('mouseup', onRelease);
-    dx = e.offsetX - e.clientX;
-    dy = e.offsetY - e.clientY;    
-    mousex = e.offsetX;
-    mousey = e.offsetX;
-    strokeStart();
-}
-function onDrag(e) {
-    var x = e.clientX + dx - canvas.width/2 ;
-    var y = e.clientY + dy - canvas.height/2;
-    stroke(x,y);
-    
-}
-function onRelease(e) {
-    document.removeEventListener('mousemove', onDrag);
-    document.removeEventListener('mouseup', onRelease);  
-    strokeEnd();
-}
-
-canvas.addEventListener('mousedown', onMouseDown);
-
-
-function strokeStart() {
-    tail = [];
-    targetCrv = [];
-    stroking = true;
-}
-
-function stroke(x,y) {
-    var m = targetCrv.length;
-    if(m>0)
-    {
-        let rx = targetCrv[m-1][0] - x;
-        let ry = targetCrv[m-1][1] - y;
-        if(rx*rx+ry*ry<50) return;        
-    }
-    targetCrv.push([x,y]);
-}
-
-function strokeEnd() {
-    analyze(targetCrv);
-    stroking = false;
-
-}
-
-function drawCurve(crv) {
-    var n = crv.length;
-    if(n<2) return;
-    ctx.beginPath();
-    ctx.moveTo(crv[0][0],crv[0][1]);
-    for(var i=1; i<n; i++) 
-        ctx.lineTo(crv[i][0],crv[i][1]);
-    ctx.stroke();
-}
-
-function drawEpicycles() {
-    if(!dft || dft.length==0) return;
-    ctx.lineWidth = 1;
-    const time = performance.now() * 0.001;
-
-    var n = dft.length;
-    var d = Math.floor(n/2);
-    let x0 = 0, y0 = 0;
-    var t = time *0.1;
-    var phi = 2*Math.PI*(t - Math.floor(t));
-    
-    for(var i=0; i<n; i++) {
-        var re1 = dft[i][0], im1 = dft[i][1];
-        var re2 = Math.cos(phi*(i-d)), im2 = Math.sin(phi*(i-d));
-        var dx = (re1*re2 - im1*im2)/n;
-        var dy = (re1*im2 + re2*im1)/n;
-        var x = x0 + dx;
-        var y = y0 + dy;
-        var r = Math.sqrt(dx*dx+dy*dy);
-        ctx.lineWidth = 1;
-        ctx.strokeStyle = "gray";
-        drawCircle(x0,y0,r);
-        ctx.strokeStyle = "black";
-        drawCircle(x0,y0,3);
-        ctx.strokeStyle = "orange";
-        ctx.lineWidth = 2;
-        drawArrow(x0,y0,x,y);
-        x0 = x;
-        y0 = y;
-    }
-    tail.push([x0,y0]);
-    var m = 1000;
-    if(tail.length>m) tail.splice(0,tail.length-m);
-}
-
-function draw() {
-    
-    if(targetCrv && targetCrv.length>2) {
-        ctx.strokeStyle = "cyan";
-        ctx.lineWidth = 5;
-        drawCurve(targetCrv);
-    }
-
-    if(!stroking) drawEpicycles();
-
-    if(tail && tail.length>2) {
-        ctx.strokeStyle = "blue";
-        ctx.lineWidth = 2;
-        drawCurve(tail);
-    }
-
-}
-
-/*
-targetCrv = [];
-var m = 20;
-for(var i=0;i<m;i++) targetCrv.push([-200+400*i/(m-1), - 200]);
-for(var i=0;i<m;i++) targetCrv.push([200,-200+400*i/(m-1)]);
-for(var i=0;i<m;i++) targetCrv.push([200-400*i/(m-1), 200]);
-for(var i=0;i<m;i++) targetCrv.push([-200,200-400*i/(m-1)]);
-analyze(targetCrv);
-*/
-
-function invtransf(w) {
-    var crv2 = [];
-    var n = w.length;
-    for(var i =0; i<n; i++) {
-        var re = 0, im = 0;
-        var phi = 2*Math.PI*i/n;
-        for(var j=0; j<n ; j++) {
-            var re1 = dft[j][0], im1 = dft[j][1];
-            var re2 = Math.cos(phi*j), im2 = Math.sin(phi*j);
-            var re3 = re1*re2 - im1*im2;
-            var im3 = re1*im2 + re2*im1;
-            re += re3;
-            im += im3;
+        re /= n
+        im /= n
+        if (k == 0) {
+            dft.center = { re, im }
+        } else {
+            dft.items.push({ re, im, k, norm: Math.sqrt(re * re + im * im) })
         }
-        crv2.push([re/n, im/n]);
     }
-    return crv2;
+    dft.items.sort((a, b) => b.norm - a.norm)
+}
+
+// --------------------------------------------------------
+// Funzione principale di disegno
+// --------------------------------------------------------
+function draw () {
+    // se è presente disegno la curva inserita dall'utente
+    if (targetCrv.length > 2) {
+        ctx.strokeStyle = 'cyan'
+        ctx.lineWidth = 5
+        drawCurve(targetCrv)
+    }
+
+    // calcolo e disegno gli epicicli se l'utente non sta disegnando ed è
+    // stata calcolata una DFT
+    if (!stroking && dft.items.length > 0) {
+        computeEpicycles()
+        drawEpicycles()
+    }
+
+    // disegno la traccia blu se c'è
+    if (tail.length > 2) {
+        ctx.strokeStyle = 'blue'
+        ctx.lineWidth = 2
+        drawCurve(tail)
+    }
+}
+
+// --------------------------------------------------------
+// Calcola la posizione e il raggio degli epicicli
+// --------------------------------------------------------
+function computeEpicycles () {
+    circles = []
+    if (dft.items.length == 0) return
+    let x = dft.center.re
+    let y = dft.center.im
+
+    // tempo in secondi dall'inizio
+    const time = performance.now() * 0.001
+    var phi = omega * time
+    const n = dft.items.length + 1 // il +1 deriva da dft.center con k=0
+    for (let i = 0; i < n - 1; i++) {
+        // aggiungo il cerchio i-esimo
+        circles.push({ x, y, r: dft.items[i].norm })
+        // calcolo le coordinate del centro del cerchio successivo
+        const re1 = dft.items[i].re
+        const im1 = dft.items[i].im
+        const k = dft.items[i].k
+        const re2 = Math.cos(phi * k)
+        const im2 = Math.sin(phi * k)
+        var dx = re1 * re2 - im1 * im2
+        var dy = re1 * im2 + re2 * im1
+        x += dx
+        y += dy
+    }
+    addPointToTail(x, y)
+}
+
+// --------------------------------------------------------
+// Disegno gli epicicli
+// --------------------------------------------------------
+function drawEpicycles () {
+    if (circles.length == 0) return
+    ctx.lineWidth = 1
+    ctx.strokeStyle = 'rgb(200,200,200,0.5)'
+    const r1 = 3
+    circles.forEach((circle, i) => {
+        const { x, y, r } = circle
+        const opacity = 1.0 / (1 + i * 0.25)
+        ctx.strokeStyle = 'rgb(100,100,100,' + opacity + ')'
+        ctx.beginPath()
+        ctx.moveTo(x + r, y)
+        ctx.arc(x, y, r, 0, 2 * Math.PI)
+        ctx.moveTo(x + r1, y)
+        ctx.arc(x, y, r1, 0, 2 * Math.PI)
+        if (i > 0) {
+            ctx.moveTo(x, y)
+            ctx.lineTo(circles[i-1].x, circles[i-1].y)
+        }
+        ctx.stroke()
+    })
+}
+
+// --------------------------------------------------------
+// gestione riga blu
+// --------------------------------------------------------
+
+// aggiunge un punto alla traccia
+// se necessario cancella i punti più vecchi
+function addPointToTail (x, y) {
+    tail.push([x, y])
+    if (tail.length > maxTailLength) {
+        tail.splice(0, tail.length - maxTailLength)
+    }
+}
+// disegno una curva
+function drawCurve (crv) {
+    var n = crv.length
+    if (n < 2) return
+    ctx.beginPath()
+    ctx.moveTo(crv[0][0], crv[0][1])
+    for (let i = 1; i < n; i++) ctx.lineTo(crv[i][0], crv[i][1])
+    ctx.stroke()
 }
